4f59bc4e2cd3145d720e29f723320ef5c5f3c121
Denis Naughton <mjolinor75@hotmail.com>
2020-09-17 19:55:30 -0400

    Generated video scaffold featuring paperclip gem

---
+++ db/schema.rb
@@ -0,0 +1,24 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_09_17_234616) do
+
+  create_table "videos", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "mp4_file_name"
+    t.string "mp4_content_type"
+    t.bigint "mp4_file_size"
+    t.datetime "mp4_updated_at"
+  end
+
+end
